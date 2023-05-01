de5ab4ada7221e14bbe261665b9cf1df9f0948ee
Bernard Calma <nard.calma@gmail.com>
2023-05-01 01:24:07 -0700

    changed course description to a list

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -15,10 +15,10 @@ const Experience = () => {
             degree: "BS",
             yearGraduate: "(Expected Garduation: 2026)",
             schoolWebsite: "https://engineering.asu.edu/",
-            description: `
-            - Learned Principles of Programing, Object Oriented Programming, Data Structures and Java.
-            - Learned System Design and construct an autonomous firefighter robot using Autodesk Fusion360 for 3D model, TinkerCAD for building circuit, Arduino for logic and MATLAB for simulations.
-            `,
+            descriptions: [
+            "Learned Principles of Programing, Object Oriented Programming, Data Structures and Java.",
+            "Learned System Design and construct an autonomous firefighter robot using Autodesk Fusion360 for 3D model, TinkerCAD for building circuit, Arduino for logic and MATLAB for simulations.",
+            ],
             image: "images/education/ASU.png",    
             projects: [
                 "Fire Fighter Swarm - ThinkerCAD, Fusion360, Arduino, C++"
@@ -30,10 +30,10 @@ const Experience = () => {
             degree: "Certificate",
             yearGraduate: "(Graduated: 2022)",
             schoolWebsite: "https://generalassemb.ly/",
-            description: `
-            - Gained 450+ hours of experience focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. 
-            - Developed a portfolio that includes individual and group projects that has been planned, designed and completed 3-4 days before deadlines.
-            `,
+            descriptions: [
+                "Gained 450+ hours of experience focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. ",
+                "Developed a portfolio that includes individual and group projects that has been planned, designed and completed 3-4 days before deadlines."
+            ],
             image: "images/education/GA.png",    
             projects: [
                 "Auction Hall (Auction Posting App) - React Native (iOS & android), PostgreSQL, Python, Flask, Javascript, CSS StyleSheets",
@@ -49,10 +49,10 @@ const Experience = () => {
             degree: "AS",
             yearGraduate: "(Graduated: 2009)",
             schoolWebsite: "https://www.sti.edu/",
-            description: `
-            - Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.
-            - Experienced building a social media application using Java and a full stack hotel reservation system using Visual Basic and Access.
-            `,
+            descriptions: [
+                "Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.",
+                "Experienced building a social media application using Java and a full stack hotel reservation system using Visual Basic and Access."
+            ],
             image: "images/education/STI.png",    
             projects: [
                 "Social Media App - Java, NetBeans",
--- src/components/experience/School.jsx
+++ src/components/experience/School.jsx
@@ -6,8 +6,13 @@ const School = (props) => {
                 <h2 className="schoolName name">{props.school.name}</h2>
                 <p className="course title">{`${props.school.course}(${props.school.degree})`}</p>
                 <p className = "yearGraduated title">{props.school.yearGraduate}</p>
-                <p className = "courseDescription description">{props.school.description}</p>
-                
+                <ul>
+                    {
+                        props.school.descriptions.map((description, index) => 
+                        <li className = "jobDescription description" key={index}>{description}</li>
+                        )
+                    }
+                </ul>
             </div>
             <div>
                 { props.school.projects.map((project, index) => <p className="schoolProject name">{project}</p>)}
