9b61fe005afda50926cab03d7084eca6b79e271f
Bernard Calma <nard.calma@gmail.com>
2023-05-02 19:03:16 -0700

    arrenged technologies used

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,7 +11,7 @@ const Project = () => {
             frontEndRepo: "https://github.com/Bernard-Calma/streamin-frontend",
             backEndRepo: "https://github.com/Bernard-Calma/streamin-backend",
             summary: "Streamin is a React-based social media app that allows users to post videos they find interesting with full CRUD capabilites. Meaning they can create, show, update, and delete the posts they make. All while being able to like the videos posted by themselves and other users, as well as comment on videos and like said comments.",
-            skills: ["HTML", "CSS", "JavaScript", "React", "MongoDB", "Express", "NodeJS"],
+            skills: ["React", "JavaScript", "NodeJS", "MongoDB", "Express", "HTML", "CSS"],
             screenshot: "images/projects/Streamin.png"
         },
         {
@@ -20,7 +20,7 @@ const Project = () => {
             frontEndRepo: "https://github.com/Bernard-Calma/spentrace-react",
             backEndRepo: "https://github.com/Bernard-Calma/spentrace_backend",
             summary: "This app can be used for simple budgeting tool, you can enter your earnings and expenses or expected cash flow everyday and will give you your how much you owe, your available spending and how much you'll need until the next expense.",
-            skills: ["HTML", "CSS", "JavaScript", "React", "MongoDB", "Express", "NodeJS"],
+            skills: ["React", "NodeJS", "JavaScript", "MongoDB", "Express", "HTML", "CSS"],
             screenshot: "images/projects/Spentrace.png"
         },
         {
@@ -29,7 +29,7 @@ const Project = () => {
             frontEndRepo: "https://github.com/Bernard-Calma/nexchapters",
             backEndRepo: "https://github.com/Bernard-Calma/nexchapter-backend",
             summary: "An app that helps you track of your reading list",
-            skills: ["React", "Redux", "Python", "Django", "Javascript", "HTML", "CSS"],
+            skills: ["React", "Redux", "Python", "Django", "PostgreSQL", "Javascript", "HTML", "CSS"],
             screenshot: "images/projects/NexChapters.png"
         }, 
         {
@@ -38,7 +38,7 @@ const Project = () => {
             frontEndRepo: "https://github.com/Bernard-Calma/project-1",
             backEndRepo: null,
             summary: "Floating Shoe is a game where you as a player will be a flying shoe and you need to avoid those feet that are trying to get into you as you move forward.",
-            skills: ["HTML", "CSS", "JavaScript", "HTML Canvas"],
+            skills: ["JavaScript", "HTML Canvas", "HTML", "CSS"],
             screenshot: "images/projects/FloatingShoe.png"
         },
         {
@@ -47,7 +47,7 @@ const Project = () => {
             frontEndRepo: "https://github.com/Bernard-Calma/tamagotchi-project",
             backEndRepo: null,
             summary: "Based on a 90's game called Tamagochi. Take care of your pet, feed, play with it. Fall inlove with it <3",
-            skills: ["HTML", "CSS", "JavaScript"],
+            skills: ["JavaScript", "HTML", "CSS"],
             screenshot: "images/projects/Tamagotchi.png"
         }
     ])
