0966d532a7eb1f9ddb13f0de396bab4f90cf933e
Bernard-Calma <nard.calma@gmail.com>
2023-09-07 21:22:09 -0700

    style(Skills): Change skills category and style
    Change skill category, font and icon style

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,42 +8,43 @@ const Home = () => {
             id: 0,
             skill: "React",
             imageLink: "https://img.icons8.com/color/256/react-native.png",
-            category: "Library"
+            category: "Front-End"
         },
         {
             id: 1,
             skill: "Redux",
             imageLink: "https://img.icons8.com/color/256/redux.png",
-            category: "Framework"
+            category: "Tools"
         },
         {
             id: 2,
             skill: "React Router",
             imageLink: "images/skills/reactRouter.png",
-            category: "Library"
+            category: "Tools"
         }, 
         {
             id: 3,
             skill: "React Native",
             imageLink: "images/skills/reactNative.png",
-            category: "Framework"
+            category: "Front-End"
         },
         {
             id: 4,
             skill: "NodeJS",
             imageLink: "https://img.icons8.com/color/256/nodejs.png",
+            category: "Back-End"
         },
         {
             id: 5,
             skill: "JavaScript",
             imageLink: "https://img.icons8.com/color/256/javascript.png",
-            category: "Programming Language"
+            category: "Programming Language",
         },
         {
             id: 6,
             skill: "jQuery",
             imageLink: "images/skills/jquery.png",
-            category: "Library"
+            category: "Tools"
         },
         {
             id: 7,
@@ -55,19 +56,19 @@ const Home = () => {
             id: 8,
             skill: "Github",
             imageLink: "https://img.icons8.com/color/256/github.png",
-            category: "Version Control"
+            category: "Tools"
         },
         {
             id: 9,
             skil: "Flask",
             imageLink: "images/skills/flask.png",
-            category: "Framework"
+            category: "Back-End"
         },
         {
             id: 10,
             skill: "Django",
             imageLink: "images/skills/django.png",
-            category: "Framework"
+            category: "Back-End"
         },
         {
             id: 11,
@@ -85,61 +86,61 @@ const Home = () => {
             id: 13,
             skill: "SQL",
             imageLink: "https://img.icons8.com/external-soft-fill-juicy-fish/256/external-sql-coding-and-development-soft-fill-soft-fill-juicy-fish.png",
-            category: "Programming Language"
+            category: "Back-End"
         },
         {
             id: 14,
             skill: "NoSQL",
             imageLink: "images/skills/nosql.png",
-            category: "Programming Language"
+            category: "Back-End"
         },
         {
             id: 15,
             skill: "PostgreSQL",
             imageLink: "https://img.icons8.com/color/256/postgreesql.png",
-            category: "Database"
+            category: "Back-End"
         },
         {
             id: 16,
             skill: "MongoDB",
             imageLink: "https://img.icons8.com/color/256/mongodb.png",
-            category: "Database"
+            category: "Back-End"
         },
         {
             id: 17,
             skill: "Access",
             imageLink: "https://img.icons8.com/color/256/microsoft-access-2019.png",
-            category: "Database"
+            category: "Back-End"
         },
         {
             id: 18,
             skill: "ExpressJS",
             imageLink: "images/skills/express.png",
-            category: "Framework"
+            category: "Back-End"
         },
         {
             id: 19,
             skill: "Bootstrap",
             imageLink: "https://img.icons8.com/color/256/bootstrap.png",
-            category: "Framework"
+            category: "Front-End"
         },
         {
             id: 20,
             skill: "Tailwind CSS",
             imageLink: "https://img.icons8.com/color/256/tailwindcss.png",
-            category: "Framework"
+            category: "Front-End"
         },
         {
             id: 21,
             skill: "Fusion360",
             imageLink: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/464bfb9c-9a76-465b-b539-f36ab9f06047/ddhhv4r-ef877702-5764-4670-bb52-eb514ecd60ed.png/v1/fill/w_512,h_512,strp/fusion_360_honeycomb_icon_by_freddiderfred_ddhhv4r-fdivlview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTEyIiwicGF0aCI6IlwvZlwvNDY0YmZiOWMtOWE3Ni00NjViLWI1MzktZjM2YWI5ZjA2MDQ3XC9kZGhodjRyLWVmODc3NzAyLTU3NjQtNDY3MC1iYjUyLWViNTE0ZWNkNjBlZC5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5ID_baqEggkjJcu_NIXYHP5bpVtj7J8DJCQ4OYOZVz4",
-            category: "Other"
+            category: "Tools"
         },
         {
             id: 22,
             skill: "Arduino",
             imageLink: "https://img.icons8.com/color/256/arduino.png",
-            category: "Framework"
+            category: "Front End"
         },
         {
             id: 23,
@@ -163,31 +164,31 @@ const Home = () => {
             id: 26,
             skill: "HTML",
             imageLink: "https://img.icons8.com/color/256/html-5.png",
-            category: "Programming Language"
+            category: "Front-End"
         },
         {
             id: 27,
             skill: "CSS",
             imageLink: "https://img.icons8.com/color/256/css3.png",
-            category: "Programming Language"
+            category: "Front-End"
         },
         {
             id: 28,
             skill: "JSON",
             imageLink: "images/skills/json.png",
-            category: "Library"
+            category: "Tools"
         },
         {
             id: 29,
             skill: "MySQL",
             imageLink: "images/skills/mysql.png",
-            category: "Database"
+            category: "Back-End"
         },
         {
             id: 30,
             skill: "EJS",
             imageLink: "images/skills/ejs.png",
-            category: "Framework"
+            category: "Tools"
         }
     ])
 
@@ -217,9 +218,9 @@ const Home = () => {
                     )}
                 </div>
 
-                <h2>Frameworks</h2>
+                <h2>Front-End</h2>
                 <div className="skillsSubList">
-                    {skillsList.map(skill => skill.category === "Framework"
+                    {skillsList.map(skill => skill.category === "Front-End"
                         ? <SkillIcon 
                         key = {skill.id}
                         skill = {skill}
@@ -228,9 +229,9 @@ const Home = () => {
                     )}
                 </div>
 
-                <h2>Libraries</h2>
+                <h2>Back-End</h2>
                 <div className="skillsSubList">
-                    {skillsList.map(skill => skill.category === "Library"
+                    {skillsList.map(skill => skill.category === "Back-End"
                         ? <SkillIcon 
                         key = {skill.id}
                         skill = {skill}
@@ -239,9 +240,9 @@ const Home = () => {
                     )}
                 </div>
 
-                <h2>Databases</h2>
+                <h2>Tools</h2>
                 <div className="skillsSubList">
-                    {skillsList.map(skill => skill.category === "Database"
+                    {skillsList.map(skill => skill.category === "Tools"
                         ? <SkillIcon 
                             key = {skill.id}
                             skill = {skill}
