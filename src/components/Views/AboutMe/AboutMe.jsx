ccf89ccc0d5f0904256dbd05254dc44f15778b10
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-04-21 02:26:47 -0700

    feat(Views): AboutMe
    Create AboutMe folder component

---
+++ src/components/Views/AboutMe/AboutMe.jsx
@@ -0,0 +1,46 @@
+const AboutMe = () => {
+    return(
+        <main class="about-me">
+            <div class="page-menu">
+            <div class="sections">
+                <div class="icon active">
+                <p>Pr</p>
+                <p>Pe</p>
+                <p>Ho</p>
+                </div>
+            </div>
+            <div class="section-content">
+                <p class="section-title"> ⮟ professional-info</p>
+                <div class="section-sub-contents">
+                <div class="sub-content">
+                    <p> experience</p>
+                    <p> hard-skills</p>
+                    <p> soft-skills</p>
+                </div>
+                </div>
+                <p class="section-title"> ⮟ contacts</p>
+                <div class="contacts">
+                <p>✉ nard.calma@gmail.com</p>
+                <p>✆ +1(123)-456-789</p>
+                </div>
+            </div>
+            </div>
+            <div class="page-view">
+            <p class="section-title"> professional-info</p>
+            <div class="section-view">
+                <p>1 /** </p>
+                <p>2 *</p>         
+                <p>3 *</p>
+                <p>4 *</p>
+                <p>5 *</p>
+                <p>6 *</p>
+                <p>7 *</p>
+                <p>8 *</p>
+                <p>9 *</p>
+            </div>
+            </div>
+      </main>
+    )
+}
+
+export default AboutMe;
--- src/components/Views/index.js
+++ src/components/Views/index.js
@@ -1,5 +1,7 @@
 import Home from "./Home/Home";
+import AboutMe from "./AboutMe/AboutMe"
 
 export {
-    Home
+    Home,
+    AboutMe
 }
