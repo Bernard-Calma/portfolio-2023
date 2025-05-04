05937e3b3f7b9a4edd0a60fb2a43918ce0a9f93d
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-05-04 02:57:20 -0700

    style(MenuOption): Filter
    Add style to filter-items

--- src/components/Views/AboutMe/components/MenuOption.jsx
+++ src/components/Views/AboutMe/components/MenuOption.jsx
@@ -2,16 +2,14 @@ const MenuOption = ({icon, label, open = false, className = "menu-option", onCli
     return (
         <div className={className} onClick={onClick}>
             <p className={`${className}_text${open ? " open" : ""}`}> <img className="icon" src={icon} alt="menu icon" /> {label}</p>
-            {
-                filterMenu?.map((filterMenuItem, index) => (
-                    <div key={index} className="sub-menu-item" onClick={filterMenuItem.onClick}>
-                        <label className="sub-menu-item-label" htmlFor={filterMenuItem.label}>
-                        <input type="checkbox" name={filterMenuItem.label}/> {filterMenuItem.label}
-                        </label>
-                        
-                    </div>
-                ))
-            }
+            <div className={`menu-option_filters${open ? " open" : ""}`}>
+                { filterMenu?.map((filterMenuItem, index) => (
+                    <label className="filter-item" key={index}>
+                        <input type="checkbox"/> {filterMenuItem.label}
+                    </label>
+                ))}
+            </div>
+
         </div>
     );
 }
